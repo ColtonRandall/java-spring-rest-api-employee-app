513c21e469006ecfd0962bfec1f17c8b52c4d45f
Colton Randall <coltonrandall.nz@gmail.com>
2025-06-08 14:47:41 +1200

    add patch mapping

--- src/main/java/com/coltonSpringApp/crudRestApi/rest/EmployeeRestController.java
+++ src/main/java/com/coltonSpringApp/crudRestApi/rest/EmployeeRestController.java
@@ -2,20 +2,25 @@ package com.coltonSpringApp.crudRestApi.rest;
 
 import com.coltonSpringApp.crudRestApi.entity.Employee;
 import com.coltonSpringApp.crudRestApi.service.EmployeeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
     private EmployeeService employeeService;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService){
-        this.employeeService = theEmployeeService;
+    public EmployeeRestController(EmployeeService theEmployeeService, ObjectMapper theObjectMapper){
+        employeeService = theEmployeeService;
+        objectMapper = theObjectMapper;
     }
 
     @GetMapping("/employees")
@@ -52,5 +57,43 @@ public class EmployeeRestController {
     @DeleteMapping("/employees/{employeeId}")
     public void deleteEmployee(@PathVariable int employeeId){
         employeeService.deleteEmployeeById(employeeId);
+    }
+
+    // add mapping for PATCH /employees/{employeeId} - patch employee - partial update
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayload){
+
+        Employee tempEmployee = employeeService.getEmployeeById(employeeId);
+
+        // throw exception if not found
+        if(tempEmployee == null) {
+            throw new RuntimeException("Employee id not found for Id: " + employeeId);
+        }
+
+        // throw exception if request body contains "id" key - we don't want users to change primary keys
+        if (patchPayload.containsKey("id")){
+            throw new RuntimeException("Employee id not allowed in request body");
+        }
+
+        Employee patchedEmployee = applyPatch(patchPayload, tempEmployee);
+
+        Employee dbEmployee = employeeService.save(patchedEmployee);
+
+        return dbEmployee;
+    }
+
+    private Employee applyPatch(Map<String, Object> patchPayload, Employee tempEmployee) {
+
+        // convert employee object to a JSON object node
+        ObjectNode employeeNode = objectMapper.convertValue(tempEmployee, ObjectNode.class);
+
+        // Convert patch payload map to json object node
+        ObjectNode patchNode = objectMapper.convertValue(patchPayload, ObjectNode.class);
+
+        // merge the patch updates into the employee node
+        employeeNode.setAll(patchNode);
+
+        return objectMapper.convertValue(employeeNode, Employee.class);
+
     }
 }
